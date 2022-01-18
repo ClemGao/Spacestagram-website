f2351ffc0135e6694fad4afd8e856eddb03f441a
yugao <gao.yu1@northeastern.edu>
2022-01-18 14:44:30 -0800

    v1.0.2

--- src/components/pages/Homepage.js
+++ src/components/pages/Homepage.js
@@ -5,8 +5,7 @@ import Item from './Item';
 function Homepage() {
 
     const [items, setItems] = useState([]);
-    const startDate = "2021-12-30";
-    const endDate = "2022-01-01";
+
     useEffect(() => {
         fetch("https://api.nasa.gov/planetary/apod?api_key=4HsyKkyTeuhUJeI9nrgTY0lIVkpaO6lyFS8bb9l0&start_date=2022-01-02&end_date=2022-01-17")
             .then(res => res.json())
